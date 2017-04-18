b5e7a2c5f5c4036d41152c0eff4447629ce80b03
LeMoussel <cnhx27@gmail.com>
2017-04-18 16:53:30 +0200

    Update Rust source

--- shutdown-agent/rust/src/main.rs
+++ shutdown-agent/rust/src/main.rs
@@ -4,8 +4,8 @@ extern crate sysfs_gpio;
 use std::process::exit;
 // https://github.com/rust-embedded/rust-sysfs-gpio
 use sysfs_gpio::{Direction, Pin};
-//use std::time::Duration;
-//use std::thread::sleep;
+use std::time::Duration;
+use std::thread::sleep;
 //use std::env;
 
 
@@ -16,26 +16,52 @@ use sysfs_gpio::{Direction, Pin};
 // https://github.com/rust-embedded/gpio-utils/blob/master/src/main.rs
 // https://github.com/inre/cupi/blob/master/examples/button.rs
 
+// https://github.com/rust-embedded/rust-sysfs-gpio
+// https://github.com/rust-embedded/gpio-utils
 
 fn main() {
-    let buzzer_pin = Pin::new(40);
+/*
+    let my_led = Pin::new(40);
+    my_led.with_exported(|| {
+        loop {
+            my_led.set_value(0).unwrap();		
+			/*match my_led.get_value() {
+						Ok(value) => println!("{}", value),
+						Err(e) => println!("ERROR: {:?}", e),
+			}*/			
+            sleep(Duration::from_millis(200));
+			
+            my_led.set_value(1).unwrap();
+            sleep(Duration::from_millis(200));
+			match my_led.get_value() {
+						Ok(value) => println!("{}", value),
+						Err(e) => println!("ERROR: {:?}", e),
+			}			
+        }
+    }).unwrap();
+*/
+
+	let buzzer_pin = Pin::new(7);
     buzzer_pin.with_exported(|| {
-        try!(buzzer_pin.set_direction(Direction::Out));        
-        Ok(())
+		sleep(Duration::from_millis(100));
+		
+		buzzer_pin.set_direction(Direction::Out).unwrap_or_else(|e| {
+			println!("Error setting GPIO direction: {:?}", e);
+			exit(1)
+		});
+		
+		buzzer_pin.set_value(0).unwrap_or_else(|e| {
+			println!("There was an error writing to the gpio: {:?}", e);
+			exit(1);
+		});
+		
+		match buzzer_pin.get_value() {
+				Ok(value) => println!("{}", value),
+				Err(e) => println!("ERROR: {:?}", e),
+		};
+		
+	    Ok(())
     });
-
-    /*buzzer_pin.set_direction(Direction::Out).unwrap_or_else(|e| {
-        println!("Error setting GPIO direction: {:?}", e);
-        exit(1)
-    });*/
-
-   /* pin.set_value(opts.value).unwrap_or_else(|e| {
-        println!("There was an error writing to the gpio: {:?}", e);
-        exit(1);
-});*/
-
-
-    println!("Hello, world!");
 }
 
 
