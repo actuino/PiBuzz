ce776dabc1915829b9b3012c110bb9cd8f208638
LeMoussel <cnhx27@gmail.com>
2017-04-19 17:28:39 +0200

    commit sur Master

--- shutdown-agent/rust/tests/test-gpio.rs
+++ shutdown-agent/rust/tests/test-gpio.rs
@@ -1,17 +1,31 @@
 
 extern crate sysfs_gpio;
 
+// https://github.com/rust-embedded/rust-sysfs-gpio
+use sysfs_gpio::{Direction, Pin};
+use sysfs_gpio::Error;
+
+pub fn setup(numgpio: u64) -> Pin {
+    return Pin::new(numgpio);
+}
+
+pub fn exit(gpio: Pin) -> Result<(), Error> {
+    return gpio.unexport();
+}
+
 #[cfg(test)]
 mod tests {
-    // https://github.com/rust-embedded/rust-sysfs-gpio
-    use sysfs_gpio::{Direction, Pin};
+    use super::*;
 
     #[test]
-    fn test_init_gpio() {
-        let gpio = Pin::new(7);
+    fn test_gpio() {
+        let gpio = setup(7);
         match gpio.export() {
             Ok(()) => assert_eq!(gpio.get_pin(), 7),
             Err(err) => println!("Gpio {} could not be exported: {}", gpio.get_pin(), err),
         }
+
+        let result = exit(gpio);
+        assert!(result.is_ok());
     }
 }
